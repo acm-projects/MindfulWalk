8eadd62d552031dd052c2665d3b989145848c4dc
Veer <veermshah@users.noreply.github.com>
2024-02-25 16:05:31 -0600

    done with explore page layout, icons, scrolling, and search bar. still need to add routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 'Explore',
                 style: GoogleFonts.raleway(
                   textStyle: TextStyle(
-                    color: Color(0xFF406440), // Use Color class with the hex value
+                    color: Color(0xFF406440),
                     fontWeight: FontWeight.bold,
                     fontSize: 45,
                   ),
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 'Enter a place or location to begin:',
                 style: GoogleFonts.raleway(
                   textStyle: TextStyle(
-                    color: Color(0xFF406440), // Use Color class with the hex value
+                    color: Color(0xFF406440),
                     fontWeight: FontWeight.normal,
                     fontSize: 20,
                     fontStyle: FontStyle.italic,
@@ -111,136 +111,198 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               SizedBox(height: 16.0),
-              Container(
-                height: 200.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFFD8D8D8),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-                  children: [
-                    // Add your image widget here
-                    Padding(
-                      padding: const EdgeInsets.only(left: 1.0, top: 5.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: Image.asset(
-                          'assets/walking-trail.jpg', // Replace with your image path
-                          width: 340.0, // Set the width as needed
-                          height: 120.0, // Set the height as needed
-                          fit: BoxFit.cover, // Adjust the fit as needed
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        height: 200.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFD8D8D8),
+                          borderRadius: BorderRadius.circular(20.0),
                         ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(16.0, 8, 16.0, 16.0),
-                        child: Text(
-                          'Walking Trails',
-                          style: GoogleFonts.raleway(
-                            textStyle: TextStyle(
-                              color: Color(0xFF5C1D45), // Use Color class with the hex value
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 1.0, top: 5.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'assets/walking-trail.jpg',
+                                  width: 340.0,
+                                  height: 120.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Container(
-                height: 200.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFFD8D8D8),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-                  children: [
-                    // Add your image widget here
-                    Padding(
-                      padding: const EdgeInsets.only(left: 1.0, top: 5.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: Image.asset(
-                          'assets/lakeside-stroll.jpg', // Replace with your image path
-                          width: 340.0, // Set the width as needed
-                          height: 120.0, // Set the height as needed
-                          fit: BoxFit.cover, // Adjust the fit as needed
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(16.0, 8, 16.0, 16.0),
-                        child: Text(
-                          'Lakeside Strolls',
-                          style: GoogleFonts.raleway(
-                            textStyle: TextStyle(
-                              color: Color(0xFF5C1D45), // Use Color class with the hex value
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(16.0, 8, 16.0, 16.0),
+                                child: Text(
+                                  'Walking Trails',
+                                  style: GoogleFonts.raleway(
+                                    textStyle: TextStyle(
+                                      color: Color(0xFF5C1D45),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Container(
-                height: 200.0,
-                decoration: BoxDecoration(
-                  color: Color(0xFFD8D8D8),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-                  children: [
-                    // Add your image widget here
-                    Padding(
-                      padding: const EdgeInsets.only(left: 1.0, top: 5.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: Image.asset(
-                          'assets/urban-ventures.jpg', // Replace with your image path
-                          width: 340.0, // Set the width as needed
-                          height: 120.0, // Set the height as needed
-                          fit: BoxFit.cover, // Adjust the fit as needed
+                      SizedBox(height: 16.0),
+                      Container(
+                        height: 200.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFD8D8D8),
+                          borderRadius: BorderRadius.circular(20.0),
                         ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(16.0, 8, 16.0, 16.0),
-                        child: Text(
-                          'Urban Ventures',
-                          style: GoogleFonts.raleway(
-                            textStyle: TextStyle(
-                              color: Color(0xFF5C1D45), // Use Color class with the hex value
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 1.0, top: 5.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'assets/lakeside-stroll.jpg',
+                                  width: 340.0,
+                                  height: 120.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                             ),
-                          ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(16.0, 8, 16.0, 16.0),
+                                child: Text(
+                                  'Lakeside Strolls',
+                                  style: GoogleFonts.raleway(
+                                    textStyle: TextStyle(
+                                      color: Color(0xFF5C1D45),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 16.0),
+                      Container(
+                        height: 200.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFD8D8D8),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 1.0, top: 5.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.asset(
+                                  'assets/urban-ventures.jpg',
+                                  width: 340.0,
+                                  height: 120.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(16.0, 8, 16.0, 16.0),
+                                child: Text(
+                                  'Urban Ventures',
+                                  style: GoogleFonts.raleway(
+                                    textStyle: TextStyle(
+                                      color: Color(0xFF5C1D45),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                
+                      SizedBox(height: 16.0),
+                    ],
+                  )
                 ),
-              ),
+              )
+
             ],
           ),
+        ),
+      ),
+
+
+
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Color(0xFF87677B),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: 0,
+          selectedItemColor: Color(0xFF5B8C5A), // we can change this if we want text under each icon
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Image.asset('assets/home.png', width: 45, height: 45),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.fromLTRB(0, 8, 30, 0),
+                child: Image.asset('assets/diary.png', width: 50, height: 50),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 35, 0),
+                child: Image.asset('assets/mapSelected.png', width: 50, height: 50),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.fromLTRB(0, 5, 30, 0),
+                child: Image.asset('assets/health.png', width: 50, height: 50),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                child: Image.asset('assets/profile.png', width: 40, height: 40),
+              ),
+              label: '',
+            ),
+          ],
+          onTap: (index) {
+            // Handle button tap
+          },
         ),
       ),
     );
