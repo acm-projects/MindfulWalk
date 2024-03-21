57ebfd11bda3a01f27717f1a9046ea720c4ef72a
Veer <veermshah@users.noreply.github.com>
2024-03-20 21:24:31 -0500

    edits to location, explore, map page

--- lib/pages/Explore.dart
+++ lib/pages/Explore.dart
@@ -4,79 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mindfulwalk/main.dart';
+import 'package:mindfulwalk/pages/Location.dart';
 import 'package:mindfulwalk/pages/MapPage.dart';
 
 void main() {
   runApp(Explore());
 }
 
-class Explore extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: ''),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Explore extends StatefulWidget {
+  const Explore({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<Explore> createState() => _ExploreState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _ExploreState extends State<Explore> {
+  final TextEditingController _textFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.fromLTRB(16.0, 25, 16, 16),
+          padding: EdgeInsets.fromLTRB(16.0, 25, 16, 0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -105,15 +55,28 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               SizedBox(height: 12.0),
               TextField(
+                controller: _textFieldController,
                 decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  prefix: SizedBox(width: 20),
-                  suffixIcon:
-                      Icon(Icons.search, color: Color(0xFF406440), size: 35),
-                ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    prefix: SizedBox(width: 20),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        String searchText = _textFieldController.text;
+                        print("Searchtext: ${searchText}");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                LocationPage(searchText: searchText),
+                          ),
+                        );
+                      },
+                      child: Icon(Icons.search,
+                          color: Color(0xFF406440), size: 35),
+                    )),
               ),
               SizedBox(height: 16.0),
               Expanded(
@@ -444,5 +407,11 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _textFieldController.dispose();
+    super.dispose();
   }
 }
